c7ab3d9034998fe15154643e299e905c2134571d
Jerome Foster II <jfoster@wlapcs.org>
2018-01-10 05:26:48 +0000

    builf keyboard commands with some easter eggs

--- sketch.js
+++ sketch.js
@@ -3,5 +3,30 @@ function setup(){
 }
 
 function draw(){
-    
+    if(keyIsPressed){
+        blendMode(LIGHTEST);
+        if(key=="r"){
+            background(255,0,0);
+        }
+        if(key=="g"){
+            background(0,255,0);
+        }
+        if(key=="b"){
+            background(0,0,255);
+        }  
+        if(keyCode==32){
+            background(255,255,255);
+         }
+        if(key=="e"){
+            background(100);
+            ellipse(mouseX, mouseY, 80, 80);
+        } 
+    }
+    else{
+        background(50);
+    }
+}
+
+function keyReleased(){
+    blendMode(NORMAL);
 }
